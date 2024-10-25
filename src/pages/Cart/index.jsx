6683a1f552dfa6a2d5a3f2d4d7789cf6a0b8f7af
guibatistella89 <guibatistella89@gmail.com>
2024-10-25 18:54:00 -0300

    Conclusão das telas do Gui

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 import { Link } from "react-router-dom";
 import logoHeader from "../../assets/logoheader.jpeg";
 import amendoimBranco from "../../assets/amendoimBranco.jpg";
 import limaozinhoBranco from "../../assets/limaozinhoBranco.jpg";
 import cachacaEscuro from "../../assets/cachacaEscuro.jpg";
 import vodkaEscuro from "../../assets/vodkaEscuro.jpg";
+import bitterEscuro from "../../assets/bitterEscuro.jpg";
+import canelinhaEscuro from "../../assets/canelinhaEscuro.jpg";
+import bananaEscuro from "../../assets/bananaEscuro.jpg";
+import limaozinhoEscuro from "../../assets/limaozinhoEscuro.jpg";
+import Navbar from "../../components/navBar.jsx";
+import printCarrinho from "../../assets/printCarrinho.jpg";
+
+
 
 import {
   CartContainer,
-  Header,
   CartContent,
   ProductItem,
   SummarySection,
@@ -16,102 +23,158 @@ import {
   DiscountSection,
   MostWantedSection,
   ProductCard,
-  QuantityControl
+  QuantityControl,
+  CartImageContainer,
+  CartImage
 } from './styles';
 
 const Cart = () => {
+  const [quantities, setQuantities] = useState({
+    amendoim: 1,
+    limaozinho: 1
+  });
+
+  const incrementQuantity = (product) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [product]: prevQuantities[product] + 1
+    }));
+  };
+
+  const decrementQuantity = (product) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [product]: prevQuantities[product] > 1 ? prevQuantities[product] - 1 : 1
+    }));
+  };
+
   return (
-
-    <CartContainer>
-      <Header>
-        <img src={logoHeader} alt="Logo Cabeçalho" />
-      </Header>
-      <CartContent>
-        <div className="cart">
-          <h2>Carrinho</h2>
-
-           {/*Primeiro item do carrinho*/} 
-          <ProductItem>
-            <img src={amendoimBranco} alt="Vodka" />
-            <div>
-              <p>Produto adicionado ao carrinho</p>
-              <h3>Amendoim 900ml</h3>
-              <p>Ingredientes: Água, álcool etílico potável, aroma de ervas</p>
-              <p className="price">R$ 15,00</p>
+    <div>
+      <div><Navbar /></div>
+      <CartContainer>
+      <CartImageContainer>
+           <CartImage src={printCarrinho} alt="Carrinho" />
+      </CartImageContainer>
+      
+        <CartContent>
+          <div className="cart">
+            {/* Primeiro item do carrinho */}
+            <ProductItem>
+              <img src={amendoimBranco} alt="Amendoim" />
+              <div className="info">
+                <p>Produto adicionado ao carrinho</p>
+                <h3>Amendoim 900ml</h3>
+                <p>Ingredientes: Água, álcool etílico potável, aroma de ervas</p>
+                <p className="price">R$ 15,00</p>
+              </div>
               <div className="actions">
                 <QuantityControl>
-                  <button>-</button>
-                  <input type="number" value={1} readOnly />
-                  <button>+</button>
+                  <button onClick={() => decrementQuantity('amendoim')}>-</button>
+                  <input type="number" value={quantities.amendoim} readOnly />
+                  <button onClick={() => incrementQuantity('amendoim')}>+</button>
                 </QuantityControl>
                 <button>Remover</button>
               </div>
-            </div>
-          </ProductItem>
+            </ProductItem>
 
-          {/*Segundo item do carrinho*/}
-          <ProductItem>
-            <img src={limaozinhoBranco} alt="Cachaça" />
-            <div>
-              <p>Produto adicionado ao carrinho</p>
-              <h3>Limaozinho 900ml</h3>
-              <p>Ingredientes: Cana, cachaça, açúcar</p>
-              <p className="price">R$ 15,00</p>
+            {/* Segundo item do carrinho */}
+            <ProductItem>
+              <img src={limaozinhoBranco} alt="Cachaça" />
+              <div className="info">
+                <p>Produto adicionado ao carrinho</p>
+                <h3>Limaozinho 900ml</h3>
+                <p>Ingredientes: Cana, cachaça, açúcar</p>
+                <p className="price">R$ 15,00</p>
+              </div>
               <div className="actions">
                 <QuantityControl>
-                  <button>-</button>
-                  <input type="number" value={1} readOnly />
-                  <button>+</button>
+                  <button onClick={() => decrementQuantity('limaozinho')}>-</button>
+                  <input type="number" value={quantities.limaozinho} readOnly />
+                  <button onClick={() => incrementQuantity('limaozinho')}>+</button>
                 </QuantityControl>
                 <button>Remover</button>
               </div>
-            </div>
-          </ProductItem>
+            </ProductItem>
 
-          {/* Cupom de desconto */}
-          <DiscountSection>
-            <input type="text" placeholder="Cupom de Desconto" />
-            <Button>APLICAR CUPOM</Button>
-          </DiscountSection>
-        </div>
-
-        {/* Resumo da compra */}
-        <SummarySection>
-          <h2>Resumo</h2>
-          <p>Valor dos produtos: <span>R$ 30,00</span></p>
-          <p>Descontos: <span>R$ 0,00</span></p>
-          <p>Frete: <span>R$ 0,00</span></p>
-          <h3>Total à prazo: <span>R$ 35,00</span></h3>
-          <h3>Total à vista no Pix: <span>R$ 30,00</span></h3>
-
-          <div className="buttons">
-            <Link to="/checkout">
-              <Button>IR PARA O PAGAMENTO</Button>
-            </Link>
-            <Link to="/">
-              <Button secondary>CONTINUAR COMPRANDO</Button>
-            </Link>
+            {/* Cupom de desconto */}
+            <DiscountSection>
+              <input type="text" placeholder="Cupom de Desconto" />
+              <Button>APLICAR CUPOM</Button>
+            </DiscountSection>
           </div>
-        </SummarySection>
-      </CartContent>
 
-      {/* Produtos mais procurados */}
-      <MostWantedSection>
-        <h2>Mais procurados</h2>
-        <div className="product-grid">
-          <ProductCard>
-            <img src={vodkaEscuro} alt="Vodka" />
-            <h3>VODKA 1764L</h3>
-            <p>R$ 299,90</p>
-          </ProductCard>
-          <ProductCard>
-            <img src={cachacaEscuro} alt="Cachaça" />
-            <h3>CACHAÇA 700ml</h3>
-            <p>R$ 280,80</p>
-          </ProductCard>
-        </div>
-      </MostWantedSection>
-    </CartContainer>
+          {/* Resumo da compra */}
+          <SummarySection>
+            <h2>Resumo</h2>
+            <p>Valor dos produtos: <span>R$ {15 * quantities.amendoim + 15 * quantities.limaozinho},00</span></p>
+            <p>Descontos: <span>R$ 0,00</span></p>
+            <p>Frete: <span>R$ 0,00</span></p>
+            <h3>Total à prazo: <span>R$ {15 * quantities.amendoim + 15 * quantities.limaozinho + 5},00</span></h3>
+            <h3>Total à vista no Pix: <span>R$ {15 * quantities.amendoim + 15 * quantities.limaozinho},00</span></h3>
+
+            <div className="buttons">
+              <Link to="/checkout">
+                <Button>IR PARA O PAGAMENTO</Button>
+              </Link>
+              <Link to="/">
+                <Button secondary>CONTINUAR COMPRANDO</Button>
+              </Link>
+            </div>
+          </SummarySection>
+        </CartContent>
+
+        {/* Produtos mais procurados */}
+        <MostWantedSection>
+          <h2>Mais procurados</h2>
+          <div className="product-grid">
+            <ProductCard>
+              <img src={vodkaEscuro} alt="Vodka" />
+              <div className="info">
+                <h3>VODKA 900ml</h3>
+                <p>R$ 15,00</p>
+              </div>
+            </ProductCard>
+            <ProductCard>
+              <img src={cachacaEscuro} alt="Cachaça" />
+              <div className="info">
+                <h3>CACHAÇA 900ml</h3>
+                <p>R$ 15,00</p>
+              </div>
+            </ProductCard>
+            <ProductCard>
+              <img src={canelinhaEscuro} alt="Canelinha" />
+              <div className="info">
+                <h3>CANELINHA 900ml</h3>
+                <p>R$ 15,00</p>
+              </div>
+            </ProductCard>
+            <ProductCard>
+              <img src={bananaEscuro} alt="Banana" />
+              <div className="info">
+                <h3>BANANA 900ml</h3>
+                <p>R$ 15,00</p>
+              </div>
+            </ProductCard>
+            <ProductCard>
+              <img src={limaozinhoEscuro} alt="Limãozinho" />
+              <div className="info">
+                <h3>LIMÃOZINHO 900ml</h3>
+                <p>R$ 15,00</p>
+              </div>
+            </ProductCard>
+            <ProductCard>
+              <img src={bitterEscuro} alt="Bitter" />
+              <div className="info">
+                <h3>BITTER 900ml</h3>
+                <p>R$ 15,00</p>
+              </div>
+            </ProductCard>
+          </div>
+        </MostWantedSection>
+      </CartContainer>
+
+
+    </div>
   );
 };
 
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -8,87 +8,83 @@ export const CartContainer = styled.div`
   background-color: #f9f9f9;
 `;
 
-export const Header = styled.header`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 140px;
-  background-color: #000000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-  z-index: 10;
-
-  img {
-    height: 100%;
-  }
-`;
-
 export const CartContent = styled.div`
   display: grid;
-  grid-template-columns: 5fr 3fr;
+  grid-template-columns: 2fr 1fr; 
   gap: 40px;
   margin-top: 160px;
   width: 100%;
   max-width: 1600px;
+  align-items: flex-start; 
+`;
+
+export const CartImageContainer = styled.div`
+  position: relative;
+  display: FLEX; 
+  justify-content: center; 
+  width: 100%;
+`;
+
+export const CartImage = styled.img`
+  width: 45%; 
+  margin-top:30px;
+  margin-bottom:-70px;
+  height: auto;
 `;
 
 export const ProductItem = styled.div`
   display: flex;
+  justify-content: space-between; /* Garante que a imagem, informações e ações fiquem separadas */
   background-color: #fff;
   padding: 20px;
   margin-bottom: 20px;
   border: 1px solid #e6e6e6;
   border-radius: 10px;
+  align-items: center; /* Alinha verticalmente os elementos */
 
   img {
     width: 120px;
     margin-right: 20px;
   }
 
-  h3 {
-    font-size: 18px;
-    color: #333;
-  }
-
-  p {
-    font-size: 14px;
-    color: #888;
-    margin: 5px 0;
-  }
-
-  .price {
-    font-size: 18px;
-    font-weight: bold;
-    color: #000;
-
-    span {
-      text-decoration: line-through;
-      color: #999;
-      margin-left: 10px;
+  .info {
+    flex-grow: 1;
+    margin-top:-35px;
+    margin-right: 20px;
+    p {
+      margin: 10px 0;
+    }
+    .price {
+      font-size: 20px;
+      font-weight: bold;
+      color: #000;
+      span {
+        text-decoration: line-through;
+        color: #999;
+        margin-left: 10px;
+      }
     }
   }
 
   .actions {
     display: flex;
-    align-items: center;
-    margin-top: 10px;
-
+    flex-direction: column;
     button {
       background-color: red;
       color: #fff;
       border: none;
       padding: 5px 10px;
       cursor: pointer;
+      margin-top: 10px;
     }
   }
 `;
 
 export const QuantityControl = styled.div`
-  display: flex;
   align-items: center;
+  margin-bottom: 10px;
+  margin-top:-20px;
+
 
   button {
     background-color: #ddd;
@@ -109,8 +105,8 @@ export const QuantityControl = styled.div`
   }
 `;
 
+
 export const SummarySection = styled.div`
-  margin-top: 65px;
   background-color: #fff;
   padding: 20px;
   border-radius: 10px;
@@ -161,7 +157,7 @@ export const Button = styled.button`
 `;
 
 export const DiscountSection = styled.div`
-  margin-top: 20px;
+  margin-top: 40px;
   display: flex;
   gap: 10px;
   width: 100%;
@@ -180,19 +176,20 @@ export const DiscountSection = styled.div`
 `;
 
 export const MostWantedSection = styled.section`
-  margin-top: 20px;
+  margin-top: 50px;
   width: 100%;
-  max-width: 800px;
+  max-width: 1600px;
 
   h2 {
-    font-size: 24px;
+    font-size: 26px;
     margin-bottom: 30px;
     color: red;
   }
 
   .product-grid {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr); /* 3 colunas */
+    grid-template-rows: repeat(2, auto); /* 2 linhas */
     gap: 20px;
   }
 `;
@@ -202,22 +199,30 @@ export const ProductCard = styled.div`
   padding: 10px;
   border: 1px solid #e6e6e6;
   border-radius: 5px;
-  text-align: center;
+  display: flex; /* Adiciona flexbox para organizar o layout */
+  //align-items: center; /* Alinha verticalmente os itens */
+  
 
   img {
-    width: 100px;
+    width: 120px;
     height: auto;
-    margin-bottom: 10px;
+    margin:15PX;
+    margin-right: 20px; /* Espaço entre a imagem e o conteúdo */
   }
 
-  h3 {
-    font-size: 16px;
-    margin: 5px 0;
-  }
+  .info {
+    margin-top:20px;
+    text-align: left; /* Alinha o texto à esquerda */
+    h3 {
+      font-size: 20px;
+      margin: 0px 0;
+    }
 
-  p {
-    font-size: 16px;
-    color: #333;
-    font-weight: bold;
+    p {
+      margin-top:20px;
+      font-size: 20px;
+      color: #333;
+      font-weight: bold;
+    }
   }
 `;
